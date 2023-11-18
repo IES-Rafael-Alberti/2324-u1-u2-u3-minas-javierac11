af0e9241528250c7488f41546f8c4cb201217911
javierac11 <javieracedocastrillon@gmail.com>
2023-11-18 12:45:55 +0100

    Refactorizar generaMinas y test del mismo

--- src/minas.py
+++ src/minas.py
@@ -36,11 +36,16 @@ def pidePosicion():
 
 def generaMinas(numero_de_minas):
     minas = set()
+    
     for numero_mina in range(numero_de_minas):
-        fila = random.randint(0, 7)
-        columna = random.randint(0, 7)
-        mina = (fila, columna)
-        minas.add(mina)
+        esta_mina = True
+        while esta_mina:
+            fila = random.randint(0, 7)
+            columna = random.randint(0, 7)
+            mina = (fila, columna)
+            if mina not in minas:
+                minas.add(mina)
+                esta_mina = False
     return minas
 
 def generarTablero():
